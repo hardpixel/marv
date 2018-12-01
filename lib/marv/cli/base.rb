53ae39dd796df0f38e0f9cedf94d2bae1d88d33f
Jonian Guveli <jonian@hardpixel.eu>
2018-12-02 01:05:00 +0200

    optimize base helper

--- lib/marv/cli/base.rb
+++ lib/marv/cli/base.rb
@@ -11,31 +11,37 @@ module Marv
       end
 
       no_commands do
+        # Print an empty message
+        def say_empty
+          self.say('')
+        end
+
         # Print general message
-        def say_message(text, color=nil, space=true)
-          text = space ? "#{text}\n\n" : "#{text}\n"
+        def say_message(text, color=nil, space_below=true, space_above=false)
+          self.say_empty if space_above
           self.say(text, color)
+          self.say_empty if space_below
         end
 
         # Print error message
-        def say_error(text, message, space=true)
-          say_message(text, :red, false)
-          say_message(message, nil, space)
+        def say_error(text, message=nil, space_below=true, space_above=false)
+          say_message(text, :red, false, space_above)
+          say_message(message, nil, space_below, false) unless message.nil?
         end
 
         # Print info message
-        def say_info(text, space=false)
-          say_message(text, :cyan, space)
+        def say_info(text, space_below=false, space_above=false)
+          say_message(text, :cyan, space_below, space_above)
         end
 
         # Print warning message
-        def say_warning(text, space=true)
-          say_message(text, :yellow, space)
+        def say_warning(text, space_below=true, space_above=false)
+          say_message(text, :yellow, space_below, space_above)
         end
 
         # Print success message
-        def say_success(text)
-          say_message("\n#{text}", :green, true)
+        def say_success(text, space_below=true, space_above=false)
+          say_message(text, :green, space_below, space_above)
         end
       end
 
