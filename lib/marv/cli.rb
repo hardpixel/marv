01750a506010a2670c5147093bffc0e3e92f0117
Jonian Guveli <jonian@hardpixel.eu>
2018-12-02 19:50:52 +0200

    add version command

--- lib/marv/cli.rb
+++ lib/marv/cli.rb
@@ -5,6 +5,14 @@ module Marv
   module CLI
     class Commands < Project
 
+      # Print version
+      desc "version", "Show version information"
+      map %w(--version -v) => :version
+      def version
+        require_relative "version"
+        puts "v#{Marv::VERSION}"
+      end
+
       # Configure marv
       desc "config", "Configure Marv projects and servers"
       long_desc "Creates a global config.rb file that can be used to auto-configure your projects and servers"
